156250a3fa100fd3113f3c7f6cc00262482e843a
Mariya <alisa-m2@yandex.ru>
2019-09-22 22:03:35 +0300

    New edit (desktop + mobile)

---
+++ build/js/main.js
@@ -0,0 +1,11 @@
+/*Mobile menu*/
+
+document.querySelector('.open-menu').addEventListener('click', () => {
+    document.querySelector('.menu_mobile').classList.add('active');
+    document.querySelector('.close-menu').classList.add('close-menu-active');
+})
+
+document.querySelector('.close-menu').addEventListener('click', () => {
+    document.querySelector('.menu_mobile').classList.remove('active');
+    document.querySelector('.close-menu').classList.remove('close-menu-active')
+})
